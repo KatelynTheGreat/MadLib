ec5895e39c6d4c9133a80116a074e1f4284b2d10
KatelynTheGreat <89535173+KatelynTheGreat@users.noreply.github.com>
2022-08-23 10:09:56 -0400

    Create madlib.py

---
+++ madlib.py
@@ -0,0 +1,8 @@
+firstName = input("Name1 ")
+otherName = input("Name2 ")
+age1 = input("A number between 10 and 15 ")
+age2 = input("A number between 10 and 15 ")
+topic = input("A sport? ")
+
+
+print(f"{firstName}'s age is {age1} and his sibling {otherName} is {age2}. The both of them love playing {topic}")
